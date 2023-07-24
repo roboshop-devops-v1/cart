8ad300c5f7bb2e6f9a78d5bcc80dcd938fb37d8f
subbum <subbu@gmail.com>
2023-07-24 06:56:58 +0100

    automation tests added

---
+++ server.test.js
@@ -0,0 +1,89 @@
+const request = require('supertest');
+const redis = require('redis-mock');
+const app = require('./server.js'); // Replace this with the actual path to your Express app file
+
+// Mock Redis client
+jest.mock('redis', () => require('redis-mock'));
+
+describe('Express App', () => {
+  let server;
+  beforeEach(() => {
+    server = app.listen(0);
+    // Mock Redis client
+    redis.createClient.mockClear();
+    redis.createClient.mockReturnValue({
+      setex: jest.fn(),
+      get: jest.fn(),
+      del: jest.fn(),
+      on: jest.fn()
+    });
+  });
+
+  afterEach((done) => {
+    server.close(done);
+  });
+
+  it('should respond with health status', async () => {
+    const response = await request(server).get('/health');
+    expect(response.status).toBe(200);
+    expect(response.body.app).toBe('OK');
+    expect(response.body.redis).toBe(false); // Assuming the Redis client is mocked and not connected
+  });
+
+  it('should respond with cart not found', async () => {
+    // Mocking RedisClient.get to return null
+    redis.createClient().get.mockImplementation((id, callback) => callback(null, null));
+
+    const response = await request(server).get('/cart/123');
+    expect(response.status).toBe(404);
+    expect(response.text).toBe('cart not found');
+  });
+
+  it('should respond with product not found', async () => {
+    // Mocking the getProduct function to return null
+    jest.spyOn(app, 'getProduct').mockResolvedValue(null);
+
+    const response = await request(server).get('/add/123/456/1');
+    expect(response.status).toBe(404);
+    expect(response.text).toBe('product not found');
+  });
+
+  it('should respond with out of stock', async () => {
+    // Mocking the getProduct function to return an out-of-stock product
+    jest.spyOn(app, 'getProduct').mockResolvedValue({ instock: 0 });
+
+    const response = await request(server).get('/add/123/456/1');
+    expect(response.status).toBe(404);
+    expect(response.text).toBe('out of stock');
+  });
+
+  it('should add items to cart', async () => {
+    // Mocking the getProduct function to return a valid product
+    jest.spyOn(app, 'getProduct').mockResolvedValue({ name: 'Product1', price: 10, instock: 5 });
+
+    // Mocking RedisClient.get to return null (no existing cart)
+    redis.createClient().get.mockImplementation((id, callback) => callback(null, null));
+
+    const response = await request(server).get('/add/123/456/2');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(1);
+    expect(response.body.items[0].sku).toBe('456');
+    expect(response.body.items[0].qty).toBe(2);
+    expect(response.body.total).toBe(20);
+    expect(response.body.tax).toBe(20 / 6); // Assuming the tax calculation is correct
+  });
+
+  it('should update cart quantity', async () => {
+    // Mocking RedisClient.get to return an existing cart
+    const existingCart = { total: 50, tax: 8.33, items: [{ sku: '456', qty: 2, price: 25, subtotal: 50 }] };
+    redis.createClient().get.mockImplementation((id, callback) => callback(null, JSON.stringify(existingCart)));
+
+    const response = await request(server).get('/update/123/456/3');
+    expect(response.status).toBe(200);
+    expect(response.body.items[0].qty).toBe(3);
+    expect(response.body.total).toBe(75);
+    expect(response.body.tax).toBe(75 / 6); // Assuming the tax calculation is correct
+  });
+
+  // Add more test cases to cover other scenarios...
+});
